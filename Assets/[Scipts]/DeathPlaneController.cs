298a424f604cdd532d1d06d4b388f5899c10e6b9
Default 101 <eorjs2020@gmail.com>
2022-11-13 19:33:58 -0500

    added DeathPlane and CheckPoints

---
+++ Assets/[Scipts]/DeathPlaneController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class DeathPlaneController : MonoBehaviour
+{
+    public Transform playerSpawnPoints;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            ReSpawn(collision.gameObject);
+        }
+    }
+
+    public void ReSpawn(GameObject go)
+    {
+        go.transform.position = playerSpawnPoints.position;
+    }
+}
